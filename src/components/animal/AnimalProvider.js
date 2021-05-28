f6e36c5bc7564fb3804a8b50ebc56a050d94493e
Jonathan Watson <thejmdw@gmail.com>
2021-05-28 14:38:04 -0400

    Chapter 3

---
+++ src/components/animal/AnimalProvider.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { useState, createContext } from "react"
+
+//The context is imported and used by individual components that need data.
+export const AnimalContext = createContext()
+
+//This component establishes what data can be used.
+export const AnimalProvider = (props) => {
+  const [animals, setAnimals] = useState([])
+
+  const getAnimals = () => {
+    return fetch("http://localhost:8088/animals?_expand=location&_expand=customer")
+    .then(res => res.json())
+    .then(setAnimals)
+  }
+
+  const addAnimal = () => {
+    return fetch("http://localhost:8088/animals", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(animalObj)
+    })
+    .then(getAnimals)
+  }
+
+  return (
+    <AnimalContext.Provider value={{
+      animals, getAnimals, addAnimal
+    }}>
+      {props.children}
+    </AnimalContext.Provider>
+  )
+}
---
+++ src/components/customer/CustomerProvider.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { useState, createContext } from "react"
+
+//The context is imported and used by individual components that need data.
+export const CustomerContext = createContext()
+
+//This component establishes what data can be used.
+export const CustomerProvider = (props) => {
+  const [customers, setCustomers] = useState([])
+
+  const getCustomers = () => {
+    return fetch("http://localhost:8088/customers")
+    .then(res => res.json())
+    .then(setCustomers)
+  }
+
+  const addCustomer = () => {
+    return fetch("http://localhost:8088/customers", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(customerObj)
+    })
+    .then(getCustomers)
+  }
+
+  return (
+    <CustomerContext.Provider value={{
+      customers, getCustomers, addCustomer
+    }}>
+      {props.children}
+    </CustomerContext.Provider>
+  )
+}
